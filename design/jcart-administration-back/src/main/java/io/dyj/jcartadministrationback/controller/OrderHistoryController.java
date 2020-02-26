9d6845c5d72b11963e3e53112ab9da5ecdb19d59
daiyujie <1123802195@qq.com>
2020-02-26 16:01:15 +0800

    add orderhistory api

---
+++ design/jcart-administration-back/src/main/java/io/dyj/jcartadministrationback/controller/OrderHistoryController.java
@@ -0,0 +1,24 @@
+package io.dyj.jcartadministrationback.controller;
+
+
+import io.dyj.jcartadministrationback.dto.in.OrderHistoryCreateInDTO;
+import io.dyj.jcartadministrationback.dto.out.OrderHistoryListOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orderhistory")
+public class OrderHistoryController {
+
+    @GetMapping("/getListByOrderId")
+   public List<OrderHistoryListOutDTO> getListByOrderId(@RequestParam Long orderId){
+       return  null;
+   }
+
+   @PostMapping("/create")
+   public Integer create(@RequestBody OrderHistoryCreateInDTO orderHistoryCreateInDTO){
+        return  null;
+   }
+
+}
