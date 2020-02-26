078d38740a1e3f371ae2599f00d5d823a3ecadc7
daiyujie <1123802195@qq.com>
2020-02-26 14:50:14 +0800

    add product api

--- design/jcart-administration-back/src/main/java/io/dyj/jcartadministrationback/controller/ProductController.java
+++ design/jcart-administration-back/src/main/java/io/dyj/jcartadministrationback/controller/ProductController.java
@@ -1,8 +1,11 @@
 package io.dyj.jcartadministrationback.controller;
 
+import io.dyj.jcartadministrationback.dto.in.ProductCreateInDTO;
 import io.dyj.jcartadministrationback.dto.in.ProductSearchInDTO;
+import io.dyj.jcartadministrationback.dto.in.ProductUpdateInDTO;
 import io.dyj.jcartadministrationback.dto.out.PageOutDTO;
 import io.dyj.jcartadministrationback.dto.out.ProductListOutDTO;
+import io.dyj.jcartadministrationback.dto.out.ProductShowOutDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,5 +16,20 @@ public class ProductController {
     public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
                                                 @RequestParam Integer pageNum){
         return  null;
+    }
+
+    @GetMapping("/getById")
+    public ProductShowOutDTO getById(@RequestParam Integer productId){
+        return  null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody ProductCreateInDTO productCreateInDTO){
+        return  null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody ProductUpdateInDTO productUpdateInDTO){
+
     }
 }
