d1159c2fc1ae825749b2f3c4ec3a3d4a551b72d1
daiyujie <1123802195@qq.com>
2020-03-02 09:06:41 +0800

    add store mybatis files

---
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/dao/OrderDetailMapper.java
@@ -0,0 +1,19 @@
+package io.dyj.jcartstoreback.dao;
+
+import io.dyj.jcartstoreback.po.OrderDetail;
+
+public interface OrderDetailMapper {
+    int deleteByPrimaryKey(Long orderId);
+
+    int insert(OrderDetail record);
+
+    int insertSelective(OrderDetail record);
+
+    OrderDetail selectByPrimaryKey(Long orderId);
+
+    int updateByPrimaryKeySelective(OrderDetail record);
+
+    int updateByPrimaryKeyWithBLOBs(OrderDetail record);
+
+    int updateByPrimaryKey(OrderDetail record);
+}
---
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/dao/OrderHistoryMapper.java
@@ -0,0 +1,17 @@
+package io.dyj.jcartstoreback.dao;
+
+import io.dyj.jcartstoreback.po.OrderHistory;
+
+public interface OrderHistoryMapper {
+    int deleteByPrimaryKey(Long orderHistoryId);
+
+    int insert(OrderHistory record);
+
+    int insertSelective(OrderHistory record);
+
+    OrderHistory selectByPrimaryKey(Long orderHistoryId);
+
+    int updateByPrimaryKeySelective(OrderHistory record);
+
+    int updateByPrimaryKey(OrderHistory record);
+}
