da043d54d3e40a88a060b3e2d01456ab850860f1
daiyujie <1123802195@qq.com>
2020-03-03 21:30:38 +0800

    add store register and login

--- design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/controller/CustomerController.java
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/controller/CustomerController.java
@@ -1,21 +1,49 @@
 package io.dyj.jcartstoreback.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import io.dyj.jcartstoreback.constant.ClientExceptionConstant;
 import io.dyj.jcartstoreback.dto.in.*;
 import io.dyj.jcartstoreback.dto.out.CustomerGetProfileOutDTO;
+import io.dyj.jcartstoreback.dto.out.CustomerLoginOutDTO;
+import io.dyj.jcartstoreback.exception.ClientException;
+import io.dyj.jcartstoreback.po.Customer;
+import io.dyj.jcartstoreback.service.CustomerService;
+import io.dyj.jcartstoreback.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @PostMapping("/register")
     public Integer register(@RequestBody CustomerRegisterInDTO customerRegisterInDTO){
-        return null;
+
+        Integer customerId = customerService.register(customerRegisterInDTO);
+        return customerId;
     }
 
     @GetMapping("/login")
-    public String login(CustomerLoginInDTO customerLoginInDTO){
-         return null;
+    public CustomerLoginOutDTO login(CustomerLoginInDTO customerLoginInDTO) throws ClientException {
+        Customer customer = customerService.getByUsername(customerLoginInDTO.getUsername());
+        if (customer == null){
+            throw new ClientException(ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRCODE, ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRMSG);
+        }
+        String encPwdDB = customer.getEncryptedPassword();
+        BCrypt.Result result = BCrypt.verifyer().verify(customerLoginInDTO.getPassword().toCharArray(), encPwdDB);
+
+        if (result.verified) {
+            CustomerLoginOutDTO customerLoginOutDTO = jwtUtil.issueToken(customer);
+            return customerLoginOutDTO;
+        }else {
+            throw new ClientException(ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRMSG);
+        }
     }
 
     @GetMapping("/getProfile")
---
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/service/CustomerService.java
@@ -0,0 +1,12 @@
+package io.dyj.jcartstoreback.service;
+
+import io.dyj.jcartstoreback.dto.in.CustomerRegisterInDTO;
+import io.dyj.jcartstoreback.po.Customer;
+
+public interface CustomerService {
+
+    Integer register(CustomerRegisterInDTO customerRegisterInDTO);
+
+    Customer getByUsername(String username);
+
+}
