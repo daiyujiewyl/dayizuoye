63edbc9804ed9c6219bd0df4c33c33d780d14da1
daiyujie <1123802195@qq.com>
2020-03-09 10:11:16 +0800

    add store customer login api

--- design/jcart-administration-back/src/main/java/io/dyj/jcartadministrationback/util/JWTUtil.java
+++ design/jcart-administration-back/src/main/java/io/dyj/jcartadministrationback/util/JWTUtil.java
@@ -1,13 +1,12 @@
-package io.cjf.jcartadministrationback.util;
 package io.dyj.jcartadministrationback.util;
+
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import io.cjf.jcartadministrationback.dto.out.AdministratorLoginOutDTO;
-import io.cjf.jcartadministrationback.po.Administrator;
-import io.cjf.jcartadministrationback.vo.AdministratorLoginVO;
+import io.dyj.jcartadministrationback.dto.out.AdministratorLoginOutDTO;
+import io.dyj.jcartadministrationback.po.Administrator;
 import io.dyj.jcartadministrationback.dto.out.AdministratorLoginOutDTO;
 import io.dyj.jcartadministrationback.po.Administrator;
 
