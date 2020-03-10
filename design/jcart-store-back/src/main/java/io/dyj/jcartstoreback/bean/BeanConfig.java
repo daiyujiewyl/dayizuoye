63c2e5d8d26a1c1bf805a932cd86843a8762186b
daiyujie <1123802195@qq.com>
2020-03-10 21:48:58 +0800

    add store customer change pwd api and page

---
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/bean/BeanConfig.java
@@ -0,0 +1,17 @@
+package io.dyj.jcartstoreback.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.SecureRandom;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public SecureRandom secureRandom(){
+
+        return new SecureRandom();
+    }
+
+}
--- design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/controller/CustomerController.java
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/controller/CustomerController.java
@@ -11,7 +11,15 @@ import io.dyj.jcartstoreback.po.Customer;
 import io.dyj.jcartstoreback.service.CustomerService;
 import io.dyj.jcartstoreback.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.SecureRandom;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/customer")
@@ -24,11 +32,22 @@ public class CustomerController {
     @Autowired
     private JWTUtil jwtUtil;
 
+    @Autowired
+    private SecureRandom secureRandom;
+
+    @Autowired(required = true)
+    private JavaMailSender mailSender;
+
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    private HashMap<String, String> emailPwdResetCodeMap = new HashMap();
+
     @PostMapping("/register")
     public Integer register(@RequestBody CustomerRegisterInDTO customerRegisterInDTO){
         Integer customerId = customerService.register(customerRegisterInDTO);
         return customerId;
-
     }
 
     @GetMapping("/login")
@@ -47,7 +66,6 @@ public class CustomerController {
             throw new ClientException(ClientExceptionConstant.CUSTOMER_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.CUSTOMER_PASSWORD_INVALID_ERRMSG);
         }
     }
-
 
     @GetMapping("/getProfile")
     public CustomerGetProfileOutDTO getProfile(@RequestAttribute Integer customerId){
@@ -72,18 +90,41 @@ public class CustomerController {
         customer.setMobile(customerUpdateProfileInDTO.getMobile());
         customer.setEmail(customerUpdateProfileInDTO.getEmail());
         customerService.update(customer);
-
     }
 
     @PostMapping("/changePwd")
     public void changePwd(@RequestBody CustomerChangePwdInDTO customerChangePwdInDTO,
-                          @RequestAttribute Integer customerId){
+                          @RequestAttribute Integer customerId) throws ClientException {
+        Customer customer = customerService.getById(customerId);
+        String encPwdDB = customer.getEncryptedPassword();
+        BCrypt.Result result = BCrypt.verifyer().verify(customerChangePwdInDTO.getOriginPwd().toCharArray(), encPwdDB);
+
+        if (result.verified) {
+            String newPwd = customerChangePwdInDTO.getNewPwd();
+            String bcryptHashString = BCrypt.withDefaults().hashToString(12, newPwd.toCharArray());
+            customer.setEncryptedPassword(bcryptHashString);
+            customerService.update(customer);
+        }else {
+            throw new ClientException(ClientExceptionConstant.CUSTOMER_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.CUSTOMER_PASSWORD_INVALID_ERRMSG);
+        }
 
     }
 
     @GetMapping("/getPwdResetCode")
-    public String getPwdResetCode(@RequestParam String email){
-        return null;
+    public void getPwdResetCode(@RequestParam String email) throws ClientException {
+        Customer customer = customerService.getByEmail(email);
+        if (customer == null){
+            throw new ClientException(ClientExceptionConstant.CUSTOMER_USERNAME_NOT_EXIST_ERRCODE, ClientExceptionConstant.CUSTOMER_USERNAME_NOT_EXIST_ERRMSG);
+        }
+        byte[] bytes = secureRandom.generateSeed(3);
+        String hex = DatatypeConverter.printHexBinary(bytes);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(email);
+        message.setSubject("jcart重置密码");
+        message.setText(hex);
+        mailSender.send(message);
+        emailPwdResetCodeMap.put("PwdResetCode"+email, hex);
     }
 
     @PostMapping("/resetPwd")
