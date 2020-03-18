3a0f9978b990989b5f4847412973289780f4f57a
daiyujie <1123802195@qq.com>
2020-03-18 14:50:15 +0800

    add store return getlist api

--- design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/controller/ReturnController.java
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/controller/ReturnController.java
@@ -1,5 +1,6 @@
 package io.dyj.jcartstoreback.controller;
 
+import com.github.pagehelper.Page;
 import io.dyj.jcartstoreback.dto.in.ReturnApplyInDTO;
 import io.dyj.jcartstoreback.dto.out.PageOutDTO;
 import io.dyj.jcartstoreback.dto.out.ReturnListOutDTO;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/return")
@@ -48,9 +51,28 @@ public class ReturnController {
 
     @GetMapping("/getList")
     public PageOutDTO<ReturnListOutDTO> getList(@RequestAttribute Integer customerId,
-                                                @RequestParam Integer pageNum){
-        return null;
+                                                @RequestParam(required = false, defaultValue = "1") Integer pageNum) {
+        Page<Return> page = returnService.getPageByCustomerId(customerId, pageNum);
+        List<ReturnListOutDTO> returnListOutDTOS = page.stream().map(aReturn -> {
+            ReturnListOutDTO returnListOutDTO = new ReturnListOutDTO();
+            returnListOutDTO.setReturnId(aReturn.getReturnId());
+            returnListOutDTO.setOrderId(aReturn.getOrderId());
+            returnListOutDTO.setCustomerId(aReturn.getCustomerId());
+            returnListOutDTO.setCustomerName(aReturn.getCustomerName());
+            returnListOutDTO.setStatus(aReturn.getStatus());
+            returnListOutDTO.setCreateTimestamp(aReturn.getCreateTime().getTime());
+            return returnListOutDTO;
+        }).collect(Collectors.toList());
+
+        PageOutDTO<ReturnListOutDTO> pageOutDTO = new PageOutDTO<>();
+        pageOutDTO.setTotal(page.getTotal());
+        pageOutDTO.setPageSize(page.getPageSize());
+        pageOutDTO.setPageNum(page.getPageNum());
+        pageOutDTO.setList(returnListOutDTOS);
+
+        return pageOutDTO;
     }
+
 
     @GetMapping("/getById")
     public ReturnShowOutDTO getById(@RequestParam Integer returnId){
--- design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/service/ReturnService.java
+++ design/jcart-store-back/src/main/java/io/dyj/jcartstoreback/service/ReturnService.java
@@ -1,10 +1,13 @@
 package io.dyj.jcartstoreback.service;
 
+import com.github.pagehelper.Page;
 import io.dyj.jcartstoreback.po.Return;
 
 public interface ReturnService {
 
     Integer create(Return ireturn);
+
+    Page<Return> getPageByCustomerId(Integer customerId,Integer pageNum);
 
 
 
